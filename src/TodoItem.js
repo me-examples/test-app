7b7c72fe3ff011ca31542af6dbcac23b55b1f853
sanjeev-bq <sanjeev.kumar@bloombergquint.com>
2020-11-29 21:18:07 +0530

    blank target

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,7 +4,7 @@ import "./style.css"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <a href={props.item.url}>
+            <a href={props.item.url} target="_blank">
               <p>{props.item.fileName}</p>
             </a>
         </div>
