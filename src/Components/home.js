fc087ee1cd6131c80e1f2520441cad16504b47b3
sanjeev-bq <sanjeev.kumar@bloombergquint.com>
2020-12-05 21:07:58 +0530

    video on home page

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import '../Styles/video-item.css';
 
-function Home() {
+const Home = ({videoDetails}) => {
 	return (
-		<div className="container-fluid text-center"> 
-		  <div className="row">
-		     This is home page 
-	    </div>
+	  <div className="flex-container">
+	    <div style={{marginTop: '20px'}}>
+		    <iframe title="home_page_viode" width="800" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" />
+			</div>
     </div>
 	)
 }
-
 export default Home;
