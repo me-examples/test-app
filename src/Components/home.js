b025511a5733cf04076d1c6804bd4a01d37e4881
sanjeev-bq <sanjeev.kumar@bloombergquint.com>
2020-12-06 15:48:21 +0530

    random home video

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import '../Styles/video-item.css';
-
+const videos = {
+	1: 'https://www.youtube.com/embed/5rB156W_fRM"',
+	2: 'https://www.youtube.com/embed/tgbNymZ7vqY',
+	3: 'https://www.youtube.com/embed/vQsoGF24glY',
+	4: 'https://www.youtube.com/embed/Bi-7pho5XB8',
+	5: 'https://www.youtube.com/embed/7sxpKhIbr0E',
+	6: 'https://www.youtube.com/embed/xnLoToJVQH4',
+	7: 'https://www.youtube.com/embed/28Kplr_9wxg',
+	8: 'https://www.youtube.com/watch?v=JMJXvsCLu6s',
+	9: 'https://www.youtube.com/embed/UZb2NOHPA2A',
+	10: 'https://www.youtube.com/embed/dWDIoW7f6js'
+}
+const getSRC = () => {
+	const key = Math.floor(Math.random() * 10) + 1;
+	return videos[key];
+}
 const Home = ({videoDetails}) => {
+	const src = getSRC();
 	return (
 	  <div className="flex-container">
 	    <div style={{marginTop: '20px'}}>
-		    <iframe title="home_page_viode" width="800" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" />
+		    <iframe
+		      title="home video"
+		      width="760"
+		      height="415"
+		      src={src}
+		      frameborder="0"
+		      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		      allowfullscreen />
 			</div>
     </div>
 	)
