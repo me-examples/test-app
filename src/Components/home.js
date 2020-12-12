3b8478588ff7e826725a81a9cd7d3442a375c7a7
sanjeev-bq <sanjeev.kumar@bloombergquint.com>
2020-12-12 12:14:23 +0530

    gmat club on home page

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import '../Styles/video-item.css';
-const videos = {
-	1: 'https://www.youtube.com/embed/5rB156W_fRM"',
-	2: 'https://www.youtube.com/embed/tgbNymZ7vqY',
-	3: 'https://www.youtube.com/embed/vQsoGF24glY',
-	4: 'https://www.youtube.com/embed/Bi-7pho5XB8',
-	5: 'https://www.youtube.com/embed/7sxpKhIbr0E',
-	6: 'https://www.youtube.com/embed/xnLoToJVQH4',
-	7: 'https://www.youtube.com/embed/28Kplr_9wxg',
-	8: 'https://www.youtube.com/watch?v=JMJXvsCLu6s',
-	9: 'https://www.youtube.com/embed/UZb2NOHPA2A',
-	10: 'https://www.youtube.com/embed/dWDIoW7f6js',
-	11: 'https://www.youtube.com/embed/O1C9zOQpKG4',
-	12: 'https://www.youtube.com/embed/wfATiSQ3Jew',
-	13: 'https://www.youtube.com/embed/kSVf0Du5-JU',
-	14: 'https://www.youtube.com/embed/nLGBEETtEPc',
-	15: 'https://www.youtube.com/embed/YVVTZgwYwVo',
-}
-const getSRC = () => {
-	const key = Math.floor(Math.random() * 15) + 1;
-	return videos[key];
-}
+// const videos = {
+// 	1: 'https://www.youtube.com/embed/5rB156W_fRM"',
+// 	2: 'https://www.youtube.com/embed/tgbNymZ7vqY',
+// 	3: 'https://www.youtube.com/embed/vQsoGF24glY',
+// 	4: 'https://www.youtube.com/embed/Bi-7pho5XB8',
+// 	5: 'https://www.youtube.com/embed/7sxpKhIbr0E',
+// 	6: 'https://www.youtube.com/embed/xnLoToJVQH4',
+// 	7: 'https://www.youtube.com/embed/28Kplr_9wxg',
+// 	8: 'https://www.youtube.com/watch?v=JMJXvsCLu6s',
+// 	9: 'https://www.youtube.com/embed/UZb2NOHPA2A',
+// 	10: 'https://www.youtube.com/embed/dWDIoW7f6js',
+// 	11: 'https://www.youtube.com/embed/O1C9zOQpKG4',
+// 	12: 'https://www.youtube.com/embed/wfATiSQ3Jew',
+// 	13: 'https://www.youtube.com/embed/kSVf0Du5-JU',
+// 	14: 'https://www.youtube.com/embed/nLGBEETtEPc',
+// 	15: 'https://www.youtube.com/embed/YVVTZgwYwVo',
+// }
+// const getSRC = () => {
+// 	const key = Math.floor(Math.random() * 15) + 1;
+// 	return videos[key];
+// }
 const Home = ({videoDetails}) => {
-	const src = getSRC();
+	// const src = getSRC();
 	return (
 	  <div className="flex-container">
-	    <div style={{marginTop: '20px'}}>
+	    { /* <div style={{marginTop: '20px'}}>
 		    <iframe
 		      title="home video"
 		      width="760"
@@ -34,7 +34,15 @@ const Home = ({videoDetails}) => {
 		      frameborder="0"
 		      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 		      allowfullscreen />
-			</div>
+			</div> */ }
+			  <iframe
+			    title="gmat club"
+			    src="https://gmatclub.com/"
+			    frameborder="0"
+			    style={{height: '1000px', width: '100%'}}
+			    height="100%"
+			    width="100%"
+			  />
     </div>
 	)
 }
